6f2e672443b8548b124b05d09e1a3560a22b70e5
Rainy1993 <474858566@qq.com>
2022-02-23 15:17:26 +0100

    added converter script. modify json output format

---
+++ converter.py
@@ -0,0 +1,16 @@
+import argparse
+import Script
+
+parser = argparse.ArgumentParser(description='Convert a PDF file to a JSON Metadata containing the word counts.')
+parser.add_argument('path', help='The path of the pdf you want to convert')
+parser.add_argument('-o', '--output', help='The type of output you wish to have. Can be console or file. For file output you should also add filename argument')
+parser.add_argument('-f', '--filename', help='The name of the file you want the output to.')
+
+args = parser.parse_args()
+
+if args.output == 'console':
+    Script.converter_pdf_json_count(args.path, file_name="")
+elif args.output == 'file':
+    if args.filename == None or args.filename == "":
+        raise parser.error('--path argument is required when output type is file')
+    Script.converter_pdf_json_count(args.path, file_name=args.filename)
